2ec81eef785663486626abf8d9ed89272d33c851
Krunal Dodiya <kunal.dodiya1@gmail.com>
2025-12-31 09:40:10 +0530

    Initial Commit

---
+++ TCC Manager/TCC_ManagerApp.swift
@@ -0,0 +1,17 @@
+//
+//  TCC_ManagerApp.swift
+//  TCC Manager
+//
+//  Created by Krunal Dodiya on 31/12/25.
+//
+
+import SwiftUI
+
+@main
+struct TCC_ManagerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
